2acb5c89523735de4592a03e13524d73e0e9ecc0
Florian Scholdei <45232454+fscholdei@users.noreply.github.com>
2020-03-23 14:05:12 +0100

    Changeover to MIT license (#1)
    * Changeover to MIT license
    * Adapt to new dispatcher test abstraction
    
    Co-authored-by: René Pfeuffer <rene.pfeuffer@cloudogu.com>

--- src/test/java/sonia/scm/legacy/ActivityLegacyResourceTest.java
+++ src/test/java/sonia/scm/legacy/ActivityLegacyResourceTest.java
@@ -1,9 +1,30 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020-present Cloudogu GmbH and Contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package sonia.scm.legacy;
 
 import com.github.sdorra.shiro.SubjectAware;
 import de.otto.edison.hal.Embedded;
-import org.jboss.resteasy.core.Dispatcher;
-import org.jboss.resteasy.mock.MockDispatcherFactory;
 import org.jboss.resteasy.mock.MockHttpRequest;
 import org.jboss.resteasy.mock.MockHttpResponse;
 import org.junit.Before;
@@ -17,6 +38,7 @@ import sonia.scm.activity.api.ActivityDto;
 import sonia.scm.activity.api.ActivityResource;
 import sonia.scm.api.v2.resources.ChangesetDto;
 import sonia.scm.api.v2.resources.PersonDto;
+import sonia.scm.web.RestDispatcher;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
@@ -39,13 +61,13 @@ public class ActivityLegacyResourceTest {
     @InjectMocks
     private ActivityLegacyResource resource;
 
-    private Dispatcher dispatcher;
+    private RestDispatcher dispatcher;
     private final MockHttpResponse response = new MockHttpResponse();
 
     @Before
     public void setUp() {
-        dispatcher = MockDispatcherFactory.createDispatcher();
-        dispatcher.getRegistry().addSingletonResource(resource);
+        dispatcher = new RestDispatcher();
+        dispatcher.addSingletonResource(resource);
     }
 
     @Test
